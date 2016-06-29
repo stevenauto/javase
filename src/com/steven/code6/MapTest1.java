4982109944f97f82aff12bda5e28deef2c6476db
window98lsq <window98lsq@sohu.com>
2016-06-29 14:24:33 +0800

    submit

---
+++ src/com/steven/code6/MapTest1.java
@@ -0,0 +1,26 @@
+package com.steven.code6;
+
+import java.util.HashMap;
+
+public class MapTest1
+{
+	public static void main(String[] args)
+	{
+		HashMap<String, String> map = new HashMap<String, String>();
+		
+		map.put("a", "zhangsan");
+		map.put("b", "lisi");
+		map.put("c", "wangwu");
+		map.put("a", "zhaoliu");
+		
+		//System.out.println(map);
+		
+		String value = (String)map.get("b");
+		System.out.println(value);
+		System.out.println("--------------");
+		
+		String value2 = (String)map.get("d");
+		System.out.println(value2);
+
+	}
+}
