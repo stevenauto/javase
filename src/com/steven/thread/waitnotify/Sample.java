c01f12436f01f14aa5bf188d22b56903d0d182c3
window98lsq <window98lsq@sohu.com>
2019-01-01 20:58:50 +0800

    aa

--- src/com/steven/thread/waitnotify/Sample.java
+++ src/com/steven/thread/waitnotify/Sample.java
@@ -6,6 +6,11 @@ public class Sample
 
 	public synchronized void increase()
 	{
+		/*必须用while，在用if的情况下，如果有二个decrease同时处于wait，increase执行完后
+		 * notify，二个decrease会减到-1，所以必须在wait后再执行一次判断（视屏104）。
+		 * 
+		 * 
+		 */
 		while (0 != number)
 		{
 			try
